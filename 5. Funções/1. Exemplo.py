bab64ffc140ded557412dc98aae51181c55cba8a
prof-carlos <128708765+prof-carlos@users.noreply.github.com>
2024-04-30 20:09:28 +0000

    Add funções

---
+++ 5. Funções/1. Exemplo.py
@@ -0,0 +1,29 @@
+import os
+
+# Solicitando dados para o usuário.    
+os.system("cls || clear")
+print("=== ===== === ")
+print("=== SENAI === ")
+print("=== ===== === ")
+nome = input("Digite seu nome: ")
+
+os.system("cls || clear")
+print("=== ===== === ")
+print("=== SENAI === ")
+print("=== ===== === ")
+idade = int(input("Digite sua idade: "))
+
+os.system("cls || clear")
+print("=== ===== === ")
+print("=== SENAI === ")
+print("=== ===== === ")
+peso = float(input("Digite seu peso: "))
+
+# Exibindo dados para o usuário.
+os.system("cls || clear")
+print("=== ===== === ")
+print("=== SENAI === ")
+print("=== ===== === ")
+print(f"Nome: {nome}")
+print(f"Idade: {idade}")
+print(f"Peso: {peso:.2f}")
